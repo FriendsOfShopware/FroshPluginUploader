28bb9924cfe2da11357cc7503a8e91522189e413
Dwayne Sharp <dwayne.sharp@4fb.de>
2019-01-28 18:39:44 +0100

    improved gethering data from account server

--- src/Components/SBP/Components/Producer.php
+++ src/Components/SBP/Components/Producer.php
@@ -4,21 +4,43 @@ namespace FroshPluginUploader\Components\SBP\Components;
 
 class Producer extends AbstractComponent
 {
-    public function getProducer(): array
+    public function getProducer($cache = false): array
     {
-        return json_decode((string) $this->client->get('/producers')->getBody(), true)[0];
+        $producer['cache'] = "var/cache/producer.json";
+        if ($cache || !file_exists($producer['cache'])) {
+            $this->updateProducerCache($producer['cache']);
+        }
+        return json_decode(file_get_contents($producer['cache']), true)[0];
     }
 
-    public function getPlugins(int $producerId): array
+    public function getPlugins($cache = false): array
     {
+        $plugins['cache'] = "var/cache/plugins.json";
+
+        $producer = $this->getProducer($cache);
+
         $query = [
             'limit' => 100,
             'offset' => 0,
             'orderBy' => 'creationDate',
             'orderSequence' => 'desc',
-            'producerId' => $producerId,
+            'producerId' => $producer['id'],
         ];
 
-        return json_decode((string) $this->client->get('/plugins?' . http_build_query($query))->getBody(), true);
+        if ($cache || !file_exists($plugins['cache'])) {
+            $this->updatePluginsCache($plugins['cache'], $query);
+        }
+
+        return json_decode(file_get_contents($plugins['cache']), true);
+    }
+
+    public function updateProducerCache($producerCache)
+    {
+        file_put_contents($producerCache, $this->client->get('/producers')->getBody());
+    }
+
+    public function updatePluginsCache($pluginCache, $query)
+    {
+        file_put_contents($pluginCache, $this->client->get('/plugins?' . http_build_query($query))->getBody());
     }
 }
