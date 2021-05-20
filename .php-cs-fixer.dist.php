be74b139e4f0a25d944343145035c9447d4cf778
max <max@swk-web.com>
2021-05-20 20:38:49 +0200

    Breaking commit message

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,6 +3,7 @@
  * This document has been generated with
  * https://mlocati.github.io/php-cs-fixer-configurator/#version:3.0.0-rc.1|configurator
  * you can change this configuration by importing this file.
+ 
  */
 return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
