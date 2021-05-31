4382b1d9277256bfda51dd103527076a6cde04b0
Shyim <6224096+shyim@users.noreply.github.com>
2021-05-31 19:33:47 +0200

    chore; Update Danger

--- .danger.php
+++ .danger.php
@@ -1,11 +1,11 @@
 <?php declare(strict_types=1);
 
 use Danger\Config;
-use Danger\Rule\CheckPhpCsFixerRule;
-use Danger\Rule\CommitRegexRule;
-use Danger\Rule\MaxCommitRule;
+use Danger\Rule\CheckPhpCsFixer;
+use Danger\Rule\CommitRegex;
+use Danger\Rule\MaxCommit;
 
 return (new Config())
-    ->useRule(new CommitRegexRule('/^(feat|fix|docs|perf|refactor|compat|chore)(\(.+\))?\:\s(.{3,})/m'))
-    ->useRule(new MaxCommitRule(1))
+    ->useRule(new CommitRegex('/^(feat|fix|docs|perf|refactor|compat|chore)(\(.+\))?\:\s(.{3,})/m'))
+    ->useRule(new MaxCommit(1))
 ;
